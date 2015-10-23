c22fcbb6089d94966600ef362e4807c7bbd7ea96
jerry4chen <jerrychen210054@gmail.com>
2015-10-24 02:16:08 +0800

    Hello Git

--- Application/src/main/java/com/example/android/camera2raw/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2raw/CameraActivity.java
@@ -24,6 +24,7 @@ import android.os.Bundle;
  */
 public class CameraActivity extends Activity {
 //add
+//    aaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
